e74ba88a17299e680057b5e03bad5e8180394b89
strangerintheq <tripmannn@gmail.com>
2017-08-30 08:56:25 +0300

    antialiasing

--- mandelbrot.glsl
+++ mandelbrot.glsl
@@ -15,19 +15,15 @@ vec2 mul(vec2 a, vec2 b) {
     return vec2(a.x * b.x - a.y * b.y, 2.0 * a.x * b.y);
 }
 
-void main(void) {
-
-	vec2 p = -resolution + 2.0*gl_FragCoord.xy;
-    p *= zoom/resolution.y;
-    p += center;
-   	vec2 z = vec2(0.0);
-    vec2 c = p;
+vec3 mandelbrot(vec2 c) {
+	c = -resolution + 2.0*c;
+    c *= zoom/resolution.y;
+    c += center;
+    vec2 z = vec2(0.);
     float it = 0.;
 
-    for (int i = 0 ; i < 1024; i++) {
-
+    for (int i = 0 ; i < 256; i++) {
         z = mul(z, z) + c;
-
     	if (dot(z, z) > 4.0) {
     	    it = float(i);
     	    break;
@@ -39,7 +35,19 @@ void main(void) {
     float al = smoothstep( -0.1, 0.0, sin(0.5*6.2831) );
     it = mix( it, sl, al );
 
-    vec3 col = 0.5 + 0.5*cos( 3.0 + it*0.05 + vec3(0.0,0.6,1.0));
-	gl_FragColor = vec4(col,1.);
+    return 0.5 + 0.5*cos( 3.0 + it*0.05 + vec3(0.0,0.6,1.0));
+}
+
+
+
+void main(void) {
+    vec2 c = gl_FragCoord.xy;
+    vec3 col;
+    col += mandelbrot(c + vec2(0.0, 0.0));
+    col += mandelbrot(c + vec2(0.0, 0.5));
+    col += mandelbrot(c + vec2(0.5, 0.0));
+    col += mandelbrot(c + vec2(0.5, 0.5));
+    col /= 4.;
+	gl_FragColor = vec4(col  ,1.);
 
 }
